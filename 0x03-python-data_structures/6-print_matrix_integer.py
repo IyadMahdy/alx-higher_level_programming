e03361ac7d97c1322b1d33864b046882e9122444
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-09 05:46:50 -0700

    Test Commit Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row[:-1]:
+        for element in row:
             print('{:d}'.format(element), end=' ')
-        print('{:d}'.format(row[-1])
+        print()
