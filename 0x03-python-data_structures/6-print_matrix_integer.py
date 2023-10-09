bd372420a92382f78c0228514334ab1825542d5a
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-09 05:48:58 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print('{:d}'.format(element), end=' ')
+    if matrix == [[]]:
         print()
+    else:
+        for row in matrix:
+            for element in row[:-1]:
+                print('{:d}'.format(element), end=' ')
+            print('{:d}'.format(row[-1]))
