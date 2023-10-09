23089f55d55a9054d904c087481899729b0e7e1b
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-09 05:37:53 -0700

    Another Fix Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return None
     new_string = ''
     for char in my_string:
-        if char is 'c' or char is 'C':
+        if char in ['c', 'C']:
             continue
         new_string += char
     return new_string
