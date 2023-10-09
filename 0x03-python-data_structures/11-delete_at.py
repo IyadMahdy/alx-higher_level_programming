a0020f1ba4146e23b2551bece518e09414c8dab1
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-09 06:38:35 -0700

    Testing Something in Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx in range(len(my_list)):
-        del my_list[idx]
-    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    return [num for i, num in enumerate(my_list) if i not idx]
