edffe14d34bad07c36a0f90364502fe18122ea2a
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-09 05:33:53 -0700

    Fixing Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace('c', '').replace('C', '')
+    new_string = ''
+    for char in my_string:
+        if char is 'c' or char is 'C':
+            continue
+        new_string += char
     return new_string
