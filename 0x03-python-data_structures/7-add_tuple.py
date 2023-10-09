8a4f2c39619e90d67022d171442d0cc015b95e0a
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-09 06:07:19 -0700

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    first_tuple = tuple_a + (0, 0)
+    second_tuple = tuple_b + (0, 0)
+
+    return (first_tuple[0] + second_tuple[0], first_tuple[1] + second_tuple[1])
