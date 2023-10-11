520157bb2392ff629f72fa9e40aa8f3f258faa19
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-11 07:37:49 -0700

    Another Fix Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_pair = max(a_dictionary.items(), key=lambda item: item[1])
-    return max_pair[0]
+    return max(a_dictionary, key=lambda key: a_dictionary[key])
