796a33bc246633adaa7bf0b1e20924593836f499
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-11 13:15:21 -0700

    Advanced Task 0

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    first = 0
+    second = 0
+    for f, s in my_list:
+        f += x * y
+        s += y
+    return f / s
