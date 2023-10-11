2ed814ebb82f79cfda3f0963d0b9b65a272cd785
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-11 07:25:17 -0700

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *= 2
-    return a_dictionary
+    return dict([(key, a_dictionary[key] * 2) for key in a_dictionary])
