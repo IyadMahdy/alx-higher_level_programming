2725e6430db7e21c235a2627ef70936551417ab2
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-11 13:19:48 -0700

    Another Stupid Fix Advanced Task 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return 0
-    first = 0
-    second = 0
+    f = 0
+    s = 0
     for x, y in my_list:
         f += x * y
         s += y
