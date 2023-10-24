40c752ee6184539c3ce569b496ebe94f671869a2
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-24 13:51:24 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines a square based on
+Private instance attribute: size
+Instantiation with size
+"""
+
+
+class Square:
+    """
+    Defines a Square
+    """
+    def __init__(self, size):
+        """
+        Constructor
+
+        Args:
+            size (int):
+        """
+        self.__size = size
