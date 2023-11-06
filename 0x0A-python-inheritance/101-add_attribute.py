502753d12d757692a56694300d566611585f2f8e
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-11-06 08:44:48 -0800

    Style Fix Advanced Task 1

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_attribute(obj_name, attr_name, value):
+    """adds a new attribute to an object if it’s possible"""
     if hasattr(obj_name, '__dict__'):
         setattr(obj_name, attr_name, value)
     else:
